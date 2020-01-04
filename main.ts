9204df109be0c429479c8834b610575e0f000fb5
RupertHuang <rupertcihao@163.com>
2020-01-04 19:57:45 +0800

    add sensor

--- main.ts
+++ main.ts
@@ -1,9 +1,156 @@
 ﻿/*
-Copyright (C): 2010-2019, SevenCode
+Copyright (C): 2018-2019, SevenCode
 modified from Rupert
 load dependency
 "SuperBit": "file:../pxt-Superbit"
 */
+
+//% color="#228B22" weight=25 icon="\uf0b2"
+namespace CrocoKit_Sensor {
+
+    const COLOR_ADD = 0X53;
+    const COLOR_REG = 0x00;
+    const COLOR_R = 0X10;
+    const COLOR_G = 0X0D;
+    const COLOR_B = 0x13;
+
+    let initialized = false;
+    let val_red = 0;
+    let val_green = 0;
+    let val_blue = 0;
+
+    export enum enGetRGB {
+        //% blockId="GetValueR" block="GetValueR"
+        GetValueR = 0,
+        //% blockId="GetValueG" block="GetValueG"
+        GetValueG = 1,
+        //% blockId="GetValueB" block="GetValueB"
+        GetValueB = 2
+    }
+
+    export enum enObstacle {
+        //% blockId="Obstacle" block="Obstacle"
+        Obstacle = 0,
+        //% blockId="NoObstacle" block="NoObstacle"
+        NoObstacle = 1
+    }
+
+    function i2cWriteData(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2);
+        buf[0] = reg;
+        buf[1] = value;
+        pins.i2cWriteBuffer(addr, buf);
+    }
+
+    function setRegConfig(): void {
+        i2cWriteData(COLOR_ADD, COLOR_REG, 0X06);
+        i2cWriteData(COLOR_ADD, 0X04, 0X41);
+        i2cWriteData(COLOR_ADD, 0x05, 0x01);
+    }
+
+    function initColorI2C(): void {
+        setRegConfig();
+        initialized = true;
+    }
+
+    function GetRGB(): void {
+        let buff_R = pins.createBuffer(2);
+        let buff_G = pins.createBuffer(2);
+        let buff_B = pins.createBuffer(2);
+
+        pins.i2cWriteNumber(COLOR_ADD, COLOR_R, NumberFormat.UInt8BE);
+        buff_R = pins.i2cReadBuffer(COLOR_ADD, 2);
+
+        pins.i2cWriteNumber(COLOR_ADD, COLOR_G, NumberFormat.UInt8BE);
+        buff_G = pins.i2cReadBuffer(COLOR_ADD, 2);
+
+        pins.i2cWriteNumber(COLOR_ADD, COLOR_B, NumberFormat.UInt8BE);
+        buff_B = pins.i2cReadBuffer(COLOR_ADD, 2);
+
+        let Red = (buff_R[1] & 0xff) << 8 | (buff_R[0] & 0xff);
+        let Green = (buff_G[1] & 0xff) << 8 | (buff_G[0] & 0xff);
+        let Blue = (buff_B[1] & 0xff) << 8 | (buff_B[0] & 0xff);
+
+        if (Red > 4500) Red = 2300;
+        if (Green > 7600) Green = 4600;
+        if (Blue > 4600) Blue = 2700;
+
+        val_red = Math.map(Red, 0, 2300, 0, 255);
+        val_green = Math.map(Green, 0, 4600, 0, 255);
+        val_blue = Math.map(Blue, 0, 2700, 0, 255);
+
+        if (val_red > 255) val_red = 255;
+        if (val_green > 255) val_green = 255;
+        if (val_blue > 255) val_blue = 255;
+
+        if (val_red == val_green && val_red == val_blue) {
+            val_red = 255;
+            val_green = 255;
+            val_blue == 255;
+        }
+        else if (val_red > val_green && val_red > val_blue) {
+            val_red = 255;
+            val_green /= 2;
+            val_blue /= 2;
+        }
+        else if (val_green > val_red && val_green > val_blue) {
+            val_green = 255;
+            val_red /= 2;
+            val_blue /= 2;
+        }
+        else if (val_blue > val_red && val_blue > val_green) {
+            val_blue = 255;
+            val_red /= 2;
+            val_green /= 2;
+        }
+    }
+
+
+    //% blockId=CrocoKit_Sensor_Light block="Light|pin %pin"
+    //% weight=100
+    //% blockGap=20
+    //% color="#228B22"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
+    export function Light(pin: AnalogPin): number {
+        let value: number;
+        value = pins.analogReadPin(pin);
+        return value;
+    }
+
+    //% blockId=CrocoKit_Sensor_Sound block="Sound|pin %pin"
+    //% weight=99
+    //% blockGap=20
+    //% color="#228B22"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
+    export function Sound(pin: AnalogPin): number {
+        let value: number;
+        value = pins.analogReadPin(pin);
+        return value;
+    }
+
+    //% blockId=CrocoKit_Sensor_GetRGBValue block="GetRGBValue|value %value"
+    //% blockGap=20
+    //% weight=98
+    //% color="#228B22"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
+    export function GetRGBValue(value: enGetRGB): number {
+        if (!initialized) {
+            initColorI2C();
+        }
+        GetRGB();
+        switch (value) {
+            case enGetRGB.GetValueR:
+                return val_red;
+            case enGetRGB.GetValueG:
+                return val_green;
+            case enGetRGB.GetValueB:
+                return val_blue;
+            default:
+                break;
+        }
+        return 0;
+    }
+
 
 //% color="#ECA40D" weight=20 icon="\uf085"
 namespace SuperBit {
